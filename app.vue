d47e09edb1735ca3cd01630f578f60f9fa8b7bbb
Olga Filipova <chudaol@gmail.com>
2022-07-11 02:14:28 +0100

    added google maps api

--- app.vue
+++ app.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="mb-20">
+    <GMapMap
+        :center="center"
+        :zoom="15"
+        :options="{
+            zoomControl: true,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            rotateControl: false,
+            fullscreenControl: true,
+          }"
+        style="width: 500px; height: 300px; margin: auto"
+    >
+      <GMapMarker
+          :key="index"
+          v-for="(marker, index) in markers"
+          :position="marker.position"
+          :clickable="true"
+          :draggable="true"
+          @click="openMarker(marker.id)"
+      >
+        <GMapInfoWindow
+            :closeclick="true"
+            @closeclick="openMarker(null)"
+            :opened="openedMarkerID === marker.id"
+        >
+          <div>{{ marker.description }}</div>
+        </GMapInfoWindow>
+      </GMapMarker>
+    </GMapMap>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      openedMarkerID: null,
+      center: { lat: 48.8773406, lng: 2.327774 },
+      markers: [
+        {
+          description: "Google France",
+          id: "1",
+          position: {
+            lat: 48.8773406,
+            lng: 2.327774,
+          },
+        },
+      ],
+    }
+  },
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    build: { transpile: ["@fawmi/vue-google-maps"] },
+    runtimeConfig: {
+        public: { GOOGLE_MAPS_API_KEY: 'AIzaSyDNnmzErBZfmFpYNBdFznfF7CVa4kvFBp0' }
+    }
 })
