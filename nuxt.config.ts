a38b0105cb56a5412a52728511e5c8da8188c59f
Datikzz <einsamejungen@gmail.com>
2022-08-30 16:21:01 +0300

    Fix by prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    build: { transpile: ["@fawmi/vue-google-maps"] },
-    runtimeConfig: {
-        public: { GOOGLE_MAPS_API_KEY: 'AIzaSyBL1pP7Y8Xq8j9EhaHpWwqkI_bpYFE8NDs' }
-    }
-})
+	build: { transpile: ['@fawmi/vue-google-maps'] },
+	runtimeConfig: {
+		public: { GOOGLE_MAPS_API_KEY: 'AIzaSyBL1pP7Y8Xq8j9EhaHpWwqkI_bpYFE8NDs' },
+	},
+});
