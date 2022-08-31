6febbdebabe851e008472fd7bd52614dd0a6f25c
Olga Filipova <chudaol@gmail.com>
2022-09-01 01:33:31 +0200

    chunk file names and entry file names using js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,10 @@ export default defineNuxtConfig({
     hooks: {
         'vite:extendConfig' (config, { isClient }) {
             if (process.env.NODE_ENV !== 'development' && isClient) {
-                config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
-                config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
+                // config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+                // config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
+                config.build.rollupOptions.output.chunkFileNames = '_nuxt/[name]-[hash].js'
+                config.build.rollupOptions.output.entryFileNames = '_nuxt/[name]-[hash].js'
             }
         }
     },
