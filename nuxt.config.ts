43cadab8eeb2f45a7f539d95337d8459d1ad989b
Olga Filipova <chudaol@gmail.com>
2022-09-01 01:42:33 +0200

    removed link css from head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,8 @@ export default defineNuxtConfig({
     hooks: {
         'vite:extendConfig' (config, { isClient }) {
             if (process.env.NODE_ENV !== 'development' && isClient) {
-                // config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
-                // config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
-                config.build.rollupOptions.output.chunkFileNames = '_nuxt/[name]-[hash].js'
-                config.build.rollupOptions.output.entryFileNames = '_nuxt/[name]-[hash].js'
+                config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+                config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
             }
         }
     },
