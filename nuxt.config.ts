2132d7efdfd24f8b9d5a77c787196b2db66209c1
Olga Filipova <chudaol@gmail.com>
2022-09-01 01:07:05 +0200

    trying chunk file names

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
+    hooks: {
+        'vite:extendConfig'(config, {isClient}) {
+            if (process.env.NODE_ENV !== 'development' && isClient) {
+                config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+                config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+            }
+        }
+    }
 });
