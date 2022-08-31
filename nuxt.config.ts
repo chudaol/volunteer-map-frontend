2429d23f5ad8e6dca52d02c148b1ca8d7ca21663
Olga Filipova <chudaol@gmail.com>
2022-09-01 01:20:47 +0200

    removed ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
     hooks: {
         'vite:extendConfig'(config, { isClient }) {
             // rc6
