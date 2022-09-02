59f1f08daef83d29f65556fbbde3a481a7765d0e
Olga Filipova <chudaol@gmail.com>
2022-09-03 00:21:16 +0200

    removed hooks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	hooks: {
-		'vite:extendConfig'(config, { isClient }) {
-			if (process.env.NODE_ENV !== 'development' && isClient) {
-				config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js';
-				config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js';
-				config.build.rollupOptions.output.assetFileNames = '[name]-[hash].js';
-			}
-		},
-	},
 	css: ['leaflet/dist/leaflet.css'],
 	module: ['nuxt-leaflet'],
 });
