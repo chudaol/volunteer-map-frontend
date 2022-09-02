0e033e4de3a10440cf7dbd8e6a465f412fa979d0
Datikzz <einsamejungen@gmail.com>
2022-09-03 01:04:10 +0300

    add extendConfig hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    build: { transpile: ["@fawmi/vue-google-maps"] },
-    runtimeConfig: {
-        public: { GOOGLE_MAPS_API_KEY: 'AIzaSyBL1pP7Y8Xq8j9EhaHpWwqkI_bpYFE8NDs' }
-    }
-})
+	build: { transpile: ['@fawmi/vue-google-maps'] },
+	runtimeConfig: {
+		public: { GOOGLE_MAPS_API_KEY: 'AIzaSyBL1pP7Y8Xq8j9EhaHpWwqkI_bpYFE8NDs' },
+	},
+	hooks: {
+		'vite:extendConfig'(config, { isClient }) {
+			if (process.env.NODE_ENV !== 'development' && isClient) {
+				config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js';
+				config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js';
+			}
+		},
+	},
+});
