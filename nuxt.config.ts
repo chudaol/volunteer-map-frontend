335ee99f4c063462956f589823e44a17aedc30b1
Datikzz <einsamejungen@gmail.com>
2022-09-03 04:01:20 +0300

    Remove buildOptimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,6 @@ export default defineNuxtConfig({
 	build: {
 		transpile: ['@fawmi/vue-google-maps'],
 		extractCSS: true,
-		optimization: {
-			splitChunks: {
-				cacheGroups: {
-					styles: {
-						name: 'styles',
-						test: /\.(css|vue)$/,
-						chunks: 'all',
-						enforce: true,
-					},
-				},
-			},
-		},
 	},
 	runtimeConfig: {
 		public: { GOOGLE_MAPS_API_KEY: 'AIzaSyBL1pP7Y8Xq8j9EhaHpWwqkI_bpYFE8NDs' },
