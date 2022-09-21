48d59c0462023d8a169f049964aea7428733a7d7
Serhii <s0936771414@gmail.com>
2022-09-21 11:30:29 +0300

    added tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  important: true,
+};
