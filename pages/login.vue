8ae8a27c79527104b2325e75bfbdb9a60cdb32f9
Serhii <s0936771414@gmail.com>
2022-09-26 10:59:27 +0300

    added login page

---
+++ pages/login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-tabs v-model="currentTab">
+      <v-tab v-for="tab in tabs" :key="tab">{{ tab }}</v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="currentTab">
+      <v-tab-item
+        ><v-row>
+          <v-col cols="24">
+            <v-text-field
+              v-model="form.email"
+              :rules="emailRules"
+              label="Email"
+              outlined
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="24">
+            <v-text-field
+              v-model="form.password"
+              :rules="passwordRules"
+              label="Password"
+              outlined
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+      <v-tab-item> content 2 </v-tab-item>
+      <v-tab-item> content 3 </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      currentTab: null,
+      tabs: ["Локальний представник", "Волонтер", "Модератор"],
+      form: {
+        email: "",
+        password: "",
+      },
+      rules: [],
+    };
+  },
+};
+</script>
