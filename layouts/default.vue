468525b6403033b875ba32501e0cba13dfcf3d9e
Datikzz <datik94@gmail.com>
2022-09-27 00:34:52 +0300

    Feature/login form (#6)
    * added login page
    
    * Modify login page according to design
    
    * Edit formatting, rename nav back handler
    
    Co-authored-by: Serhii <s0936771414@gmail.com>
    Co-authored-by: Datikzz <einsamejungen@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,18 +24,10 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        v-if="drawer"
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn v-if="drawer" icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
@@ -45,27 +37,17 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -73,29 +55,34 @@
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
+  name: "DefaultLayout",
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: true,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
         },
         {
-          icon: 'mdi-plus-circle',
-          title: 'Add Marker',
-          to: '/addmarker'
-        }
+          icon: "mdi-plus-circle",
+          title: "Add Marker",
+          to: "/addmarker",
+        },
+        {
+          icon: "mdi-account-details",
+          title: "Login",
+          to: "/login",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Volunteer Map'
-    }
-  }
-}
+      title: "Volunteer Map",
+    };
+  },
+};
 </script>
